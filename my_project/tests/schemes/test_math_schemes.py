425b886547631bbe35fb9e02a0c54c02a91938b1
Felipe Míguez Kautzmann <fkautzmann@immomio.de>
2024-07-31 16:32:03 +0200

    feat: added unittests and integration tests

---
+++ my_project/tests/schemes/test_math_schemes.py
@@ -0,0 +1,71 @@
+import pytest
+from pydantic_core._pydantic_core import ValidationError
+
+from Python_Challenges.my_project.schemes.maths import InputMax, NumeroPrimo
+
+
+def test_inputmax():
+    # Given the input and the expected result
+    int_input = 1
+    expected_result = 1
+
+    # When creating the InputMax object
+    result = InputMax(choice=int_input)
+
+    # Then the result must match the expected one
+    assert result.choice == expected_result
+
+
+def test_str_inputmax():
+    # Given the input and the expected result
+    int_input = '1'
+    expected_result = 1
+
+    # When creating the InputMax object
+    result = InputMax(choice=int_input)
+
+    # Then the result must match the expected one
+    assert result.choice == expected_result
+
+
+def test_input_max_wrong_input():
+    # Given the input and the expected result
+    int_input = '-1'
+
+    # When creating the InputMax object
+
+    # Then the exception is raised
+    with pytest.raises(ValueError):
+        result = InputMax(choice=int_input)
+
+
+def test_numero_primo():
+    # Given the input number and the expected result
+    input_number = 3
+    expected_result = 3
+
+    # When creating a prime number object
+    result = NumeroPrimo(number=input_number)
+
+    # Then must match with the expected_result
+    assert result.number == expected_result
+
+
+def test_wrong_numero_primo():
+    # Given the input number and the expected result
+    input_number = ['a']
+    expected_result = 'lolailo'
+
+    # When creating a prime number object
+
+    # Then the exception is raised
+    with pytest.raises(ValidationError):
+        result = NumeroPrimo(number=input_number)
+
+
+def test_set_prime_numbers():
+    ...
+    # Given
+
+    # When
+    # Then
